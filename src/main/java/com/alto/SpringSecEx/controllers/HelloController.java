926340df497ba5739f0b3cbaa2a29d35ebc34b12
Ruslan <garridubua@gmail.com>
2025-08-31 15:06:58 +0300

    Crsf endpoint + students endpoints

---
+++ src/main/java/com/alto/SpringSecEx/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.alto.SpringSecEx.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+
+        return "Welcome to Sea of thieves " + request.getSession().getId();
+    }
+
+}
