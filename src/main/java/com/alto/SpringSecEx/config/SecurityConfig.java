800e7b4561c3c13bc3c5a82c064caf06e368f237
Ruslan <garridubua@gmail.com>
2025-09-02 21:40:55 +0300

    Validating JWT token

--- src/main/java/com/alto/SpringSecEx/config/SecurityConfig.java
+++ src/main/java/com/alto/SpringSecEx/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.alto.SpringSecEx.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,12 +22,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
+
+    private final JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -40,7 +45,8 @@ public class SecurityConfig {
                 session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS
                 )
-        ).build();
+        ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
         //http.formLogin(Customizer.withDefaults());
     }
     @Bean
